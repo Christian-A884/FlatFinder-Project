7f26dab6d205e2d2b4bbda03fd3ba859d42630f8
Christian-A884 <christian.albu@yahoo.com>
2024-04-17 15:44:11 +0300

    make login page

--- flat_finder_project/src/pages/Login.tsx
+++ flat_finder_project/src/pages/Login.tsx
@@ -1,9 +1,81 @@
+import { SubmitHandler, useForm } from "react-hook-form";
 
+type FormFields = {
+  email: string;
+  password: string;
+};
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormFields>();
 
-export default Login
+  const onSubmit: SubmitHandler<FormFields> = (data) => console.log(data);
+
+  return (
+    
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col justify-center items-center w-full h-auto my-48 px-4 sm:px-20 md:px-52 mx-auto max-w-[900px] "
+      >
+        <h3 className="m-4 text-[16px] text-center font-semibold">Login to your account</h3>
+        <div className="flex flex-col justify-center items-start w-full text-xs gap-1 ">
+          <label htmlFor="email">Email address</label>
+          <input
+            {...register("email", {
+              required: "Email is required",
+              validate: {
+                isEmail: (value) =>
+                  /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(value) ||
+                  "Invalid email format",
+              },
+            })}
+            className="h-8 w-full border border-gray-500 rounded-md pl-2 text-xs placeholder:text-xs"
+            type="text"
+            placeholder="Email address"
+            id="email"
+          />
+          <p className="text-[10px] h-6 text-red-600">
+            {errors.email && (errors.email.message as string)}
+          </p>
+        </div>
+        <div className="flex flex-col justify-center items-start w-full  text-xs gap-1">
+          <label htmlFor="password">Password</label>
+          <input
+            {...register("password", {
+              required: "Password is required",
+              minLength: {
+                value: 6,
+                message: "Password length must be at least 6 charachters",
+              },
+              pattern: {
+                value:
+                  /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@#$%^&*!])[a-zA-Z\d@#$%^&*!]{6,}$/,
+
+                message:
+                  "Password must contain at least one uppercase letter,one number and one special character",
+              },
+            })}
+            className="h-8 w-full border border-gray-500 rounded-md pl-2 text-xs placeholder:text-xs"
+            type="text"
+            placeholder="Password"
+            id="password"
+          />
+          <p className="text-[10px] h-6 text-red-600">
+            {errors.password && (errors.password.message as string)}
+          </p>
+        </div>
+        <button
+        className="bg-[#F1654D] text-white text-xs  font-semibold w-full h-8 rounded-md"
+        type="submit"
+      >
+        Login
+      </button>
+      </form>
+    
+  );
+};
+
+export default Login;
--- flat_finder_project/src/pages/Register.tsx
+++ flat_finder_project/src/pages/Register.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 
-type FormFileds = {
+type FormFields = {
   firstName: string;
   lastName: string;
   email: string;
@@ -16,12 +16,12 @@ const Register = () => {
     handleSubmit,
     watch,
     formState: { errors },
-  } = useForm<FormFileds>();
+  } = useForm<FormFields>();
 
   const [birthDate, setBirthDate] = useState('')
  
 
-  const onSubmit:SubmitHandler<FormFileds> = (data) => {console.log(data)};
+  const onSubmit:SubmitHandler<FormFields> = (data) => {console.log(data)};
 
   const password = watch('password')
 
@@ -38,57 +38,54 @@ const Register = () => {
   
 
   const handleInputChange = (e:React.ChangeEvent<HTMLInputElement>) =>
-    {setBirthDate(e.target.value)
-   
-    }
+    {setBirthDate(e.target.value)}
 
 
- 
   return (           
     <form
       onSubmit={handleSubmit(onSubmit)}
-      className="flex flex-col justify-center items-center max-w[1440px] h-full gap-5 mx-auto mt-36 mb-36"
+      className="flex flex-col justify-center items-center w-full h-full px-4 sm:px-20 md:px-52 mx-auto max-w-[900px] "
     >
-      <h1 className="m-4 text-2xl font-semibold">Create your account</h1>
-      <div className="flex flex-col justify-center items-start w-[900px] ">
+      <h3 className="m-4 text-[16px] text-center font-semibold">Create your account</h3>
+      <div className="flex flex-col justify-center items-start w-full text-xs gap-1">
         <label htmlFor="firstName">Firstname</label>
         <input
           {...register("firstName", {
-            required: { value: true, message: "This filed is required" },
+            required: { value: true, message: "This field is required" },
             minLength: {
               value: 2,
               message: "Firstname should have at least 2 characters",
             },
           })}
-          className="h-10 w-full border border-gray-500 rounded-md pl-2 text-sm"
+          className="h-8 w-full border border-gray-500 rounded-md pl-2 text-xs placeholder:text-xs"
           type="text"
           placeholder="First Name"
           id="firstName"
         />
-        <p className="text-sm h-6 text-red-600">
+        <p className="text-[10px] h-6 text-red-600">
           {errors.firstName && (errors.firstName.message as string)}
         </p>
       </div>
-      <div className="flex flex-col justify-center items-start w-[900px] ">
+      <div className="flex flex-col justify-center items-start w-full  text-xs gap-1">
         <label htmlFor="lasttName">Lastname</label>
         <input
           {...register("lastName", {
-            required: { value: true, message: "This filed is required" },
+            required: { value: true, message: "This field is required" },
             minLength: {
               value: 2,
               message: "Lastname should have at least 2 characters",
             },
           })}
-          className="h-10 w-full border border-gray-500 rounded-md pl-2 text-sm"
+          className="h-8 w-full border border-gray-500 rounded-md pl-2 text-xs placeholder:text-xs"
           type="text"
           placeholder="Last Name"
           id="lastName"
         />
-        <p className="text-sm h-6 text-red-600">
+        <p className="text-[10px] h-6 text-red-600">
           {errors.firstName && (errors.firstName.message as string)}
         </p>
       </div>
-      <div className="flex flex-col justify-center items-start w-[900px] ">
+      <div className="flex flex-col justify-center items-start w-full text-xs gap-1">
         <label htmlFor="email">Email address</label>
         <input
           {...register("email", {
@@ -99,16 +96,16 @@ const Register = () => {
                 "Invalid email format",
             },
           })}
-          className="h-10 w-full border border-gray-500 rounded-md pl-2 text-sm"
+          className="h-8 w-full border border-gray-500 rounded-md pl-2 text-xs placeholder:text-xs"
           type="text"
           placeholder="Email address"
           id="email"
         />
-        <p className="text-sm h-6 text-red-600">
+        <p className="text-[10px] h-6 text-red-600">
           {errors.email && (errors.email.message as string)}
         </p>
       </div>
-      <div className="flex flex-col justify-center items-start w-[900px] ">
+      <div className="flex flex-col justify-center items-start w-full  text-xs gap-1">
         <label htmlFor="password">Password</label>
         <input
           {...register("password", {
@@ -125,16 +122,16 @@ const Register = () => {
                 "Password must contain at least one uppercase letter,one number and one special character",
             },
           })}
-          className="h-10 w-full border border-gray-500 rounded-md pl-2 text-sm"
+          className="h-8 w-full border border-gray-500 rounded-md pl-2 text-xs placeholder:text-xs"
           type="text"
           placeholder="Password"
           id="password"
         />
-        <p className="text-sm h-6 text-red-600">
+        <p className="text-[10px] h-6 text-red-600">
           {errors.password && (errors.password.message as string)}
         </p>
       </div>
-      <div className="flex flex-col justify-center items-start w-[900px] ">
+      <div className="flex flex-col justify-center items-start w-full text-xs gap-1">
         <label htmlFor="confirmPassword">Confirm Password</label>
         <input {...register("confirmPassword", {
             
@@ -143,15 +140,15 @@ const Register = () => {
             }
       
           )}
-          className="h-10 w-full border border-gray-500 rounded-md pl-2 text-sm"
+          className="h-8 w-full border border-gray-500 rounded-md pl-2 text-xs placeholder:text-xs"
           type="text"
           placeholder="Confirm Password"
           id="confirmPassword"
         />
-        <p className="text-sm h-6 text-red-600">
+        <p className="text-[10px] h-6 text-red-600">
           {errors.confirmPassword && (errors.confirmPassword.message as string)}</p>
       </div>
-      <div className="flex flex-col justify-center items-start w-[900px] ">
+      <div className="flex flex-col justify-center items-start w-full text-xs gap-1">
         <label htmlFor="birthday">Birthday</label>
         <input
         {...register('birthday',{
@@ -159,18 +156,18 @@ const Register = () => {
         
           validate: value => calculateAge(value) >= 18 && calculateAge(value) <= 120 || "Age must be between 18 and 120"
         })}
-          className="h-10 w-full border border-gray-500 rounded-md pl-2 text-sm"
+          className="h-8 w-full border border-gray-500 rounded-md pl-2 text-xs placeholder:text-xs"
           type="date"
           placeholder="Birthday"
           name="birthday"
           onChange={handleInputChange}
           value={birthDate}
         />
-        <p className="text-sm h-6 text-red-600">
+        <p className="text-[10px] h-6 text-red-600">
           {errors.birthday && (errors.birthday.message as string)}</p>
       </div>
       <button
-        className="bg-[#F1654D] text-white text-lg  font-semibold w-[900px] h-10 rounded-md"
+        className="bg-[#F1654D] text-white text-xs  font-semibold w-full h-8 rounded-md"
         type="submit"
       >
         Create my account
